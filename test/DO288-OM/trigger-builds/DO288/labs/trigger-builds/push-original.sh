d1d862c650e54461017ba8592f78b93b9f62a5fe
policier <oweni2002@yahoo.fr>
2022-07-11 16:30:46 +0200

    add comment

--- test/DO288-OM/trigger-builds/DO288/labs/trigger-builds/push-original.sh
+++ test/DO288-OM/trigger-builds/DO288/labs/trigger-builds/push-original.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /usr/local/etc/ocp4.config
-cd /home/student/DO288/labs/trigger-builds
+#source /usr/local/etc/ocp4.config
+#cd /home/student/DO288/labs/trigger-builds
 VERSION="original"
-sudo XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR skopeo copy oci-archive:php-73-ubi8-${VERSION}.tar.gz docker://quay.io/${RHT_OCP4_QUAY_USER}/php-73-ubi8:latest
+sudo  skopeo copy oci-archive:php-73-ubi8-${VERSION}.tar.gz docker://quay.io/omougoue/php-73-ubi8:latest
